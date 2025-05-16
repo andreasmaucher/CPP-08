5931975ce3de38c6e4472e8378840140200fb45a
amaucher <andreas-maucher@gmx.de>
2025-05-16 23:12:17 +0200

    added ex02

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -3,27 +3,40 @@
 
 #include <stack>
 
+// the entire class is a template so it can work with any type
+// inherits from std::stack that uses std::deque as its underlying container
+// template classes need their entire implementation in the header file for compilation reasons
 template<typename T>
 class MutantStack : public std::stack<T>
 {
 public:
-    // Orthodox Canonical Form
+    // default constructor
     MutantStack() : std::stack<T>() {}
+    // copy constructor
     MutantStack(const MutantStack& other) : std::stack<T>(other) {}
+    // copy assignment operator
     MutantStack& operator=(const MutantStack& other) {
         if (this != &other)
             std::stack<T>::operator=(other);
         return *this;
     }
+    // destructor
     ~MutantStack() {}
 
     // Iterator typedefs - using the underlying container's iterators
+    /*
+    There are four iterator types:
+        iterator - regular iterator (can read and modify elements)
+        const_iterator - constant iterator (can only read elements)
+        reverse_iterator - reverse iterator (goes backwards, can read and modify)
+        const_reverse_iterator - constant reverse iterator (goes backwards, can only read)
+    */
     typedef typename std::stack<T>::container_type::iterator iterator;
     typedef typename std::stack<T>::container_type::const_iterator const_iterator;
     typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
     typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
 
-    // Iterator functions
+    // Iterator functions for begin and end and it's reverse versions
     iterator begin() { return this->c.begin(); }
     iterator end() { return this->c.end(); }
     const_iterator begin() const { return this->c.begin(); }
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -2,74 +2,57 @@
 #include <iostream>
 #include <list>
 
+/*
+std::stack container is one of the only containers that is not iterable
+*/
+
 int main()
 {
-    // Test with MutantStack
-    std::cout << "=== Testing MutantStack ===" << std::endl;
+    std::cout << "--- Test from subject ---" << std::endl;
     MutantStack<int> mstack;
-
     mstack.push(5);
     mstack.push(17);
-
-    std::cout << "Top: " << mstack.top() << std::endl;
-
+    std::cout << "top: " << mstack.top() << std::endl;
     mstack.pop();
-
-    std::cout << "Size: " << mstack.size() << std::endl;
-
+    std::cout << "size: " << mstack.size() << std::endl;
     mstack.push(3);
     mstack.push(5);
     mstack.push(737);
+    //[...]
     mstack.push(0);
-
-    std::cout << "Iterating through MutantStack:" << std::endl;
     MutantStack<int>::iterator it = mstack.begin();
     MutantStack<int>::iterator ite = mstack.end();
-
     ++it;
     --it;
-
+    std::cout << "- printing values -" << std::endl;
     while (it != ite)
     {
-        std::cout << *it << std::endl;
+        std::cout << "value: " << *it << std::endl;
         ++it;
     }
-
-    // Test copy constructor and assignment
-    std::cout << "\n=== Testing copy and assignment ===" << std::endl;
-    MutantStack<int> s(mstack);
-    std::cout << "Copied stack size: " << s.size() << std::endl;
-
-    // Test with std::list for comparison
-    std::cout << "\n=== Testing std::list ===" << std::endl;
+    std::stack<int> s(mstack);
+    
+    std::cout << "\n--- Testing with list ---" << std::endl;
     std::list<int> lst;
-
     lst.push_back(5);
     lst.push_back(17);
-
-    std::cout << "Top: " << lst.back() << std::endl;
-
+    std::cout << "top: " << lst.back() << std::endl;
     lst.pop_back();
-
-    std::cout << "Size: " << lst.size() << std::endl;
-
+    std::cout << "size: " << lst.size() << std::endl;
     lst.push_back(3);
     lst.push_back(5);
     lst.push_back(737);
+    //[...]
     lst.push_back(0);
-
-    std::cout << "Iterating through list:" << std::endl;
     std::list<int>::iterator lst_it = lst.begin();
     std::list<int>::iterator lst_ite = lst.end();
-
     ++lst_it;
     --lst_it;
-
+    std::cout << "- printing values -" << std::endl;
     while (lst_it != lst_ite)
     {
-        std::cout << *lst_it << std::endl;
+        std::cout << "value: " << *lst_it << std::endl;
         ++lst_it;
     }
-
-    return 0;
+    return (0);
 }
